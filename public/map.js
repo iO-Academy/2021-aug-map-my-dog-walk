cfe82da65912c1776884e2f666859f584640f4c1
Jake Rooms <jakeprooms@outlook.com>
2021-12-13 15:49:58 +0000

    chenged getElementById to query selector

--- public/map.js
+++ public/map.js
@@ -3,7 +3,7 @@ function myMap() {
         center: {lat: 51.508742, lng: -0.120850},
         zoom:5
     };
-    const map = new google.maps.Map(document.getElementById("map"),mapProp);
+    const map = new google.maps.Map(document.querySelector("#map"),mapProp);
 
     new google.maps.Marker({
         position: {lat: 51.508742, lng: -0.120850},
