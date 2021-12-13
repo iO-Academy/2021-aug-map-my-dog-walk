dc61a52a5861acb15c8616c53e83e17169765391
Jake Rooms <jakeprooms@outlook.com>
2021-12-13 16:14:23 +0000

    saved changes

--- src/Controllers/HomePageController/index.js
+++ src/Controllers/HomePageController/index.js
@@ -1,10 +1,8 @@
 const DbService = require('../../Services/DbService');
 
 function renderHomePage(request, response){
-    response.render('Home')
+    response.render('Home', )
 }
-
-
 
 module.exports.renderHomePage = renderHomePage
 
