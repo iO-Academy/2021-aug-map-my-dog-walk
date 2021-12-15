589528b5bc289ca03c03cc80243a5b738067779d
jesshamby <jesshamby99@gmail.com>
2021-12-15 14:56:26 +0000

    Removed unecessary require

--- src/Controllers/MarkerAPIController/index.js
+++ src/Controllers/MarkerAPIController/index.js
@@ -1,5 +1,4 @@
 const DbService = require("../../Services/DbService");
-const {addAdditionalRouteMarkers} = require("../../Services/DbService");
 
 async function getAllStartMarkersController(request, response) {
     const collection = await DbService.connectToDb();
