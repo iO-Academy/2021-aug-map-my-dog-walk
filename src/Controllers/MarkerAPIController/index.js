a6c112276859ded2ffd0a38f816933aa92b5fafd
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-12-15 13:51:50 +0000

    added verification to check for empty markers array

--- src/Controllers/MarkerAPIController/index.js
+++ src/Controllers/MarkerAPIController/index.js
@@ -3,8 +3,19 @@ const DbService = require("../../Services/DbService");
 async function getAllStartMarkersController(request, response) {
     const collection = await DbService.connectToDb();
     const markers = await DbService.getAllStartMarkers(collection);
-    response.json(markers);
+    if (markers.length !== 0) {
+        response.json({
+            success: true,
+            message: 'Successfully found markers',
+            data: markers
+        });
+    } else {
+        response.json({
+            success: false,
+            message: 'No markers returned :(',
+            data: markers
+        });
+    }
 }
-
 
 module.exports.getAllStartMarkersController = getAllStartMarkersController
