0b2887b7de635e4f9a66959894a03f61af18e36a
jesshamby <jesshamby99@gmail.com>
2021-12-15 15:03:41 +0000

    Updated request.params to request.body

--- src/Controllers/MarkerAPIController/index.js
+++ src/Controllers/MarkerAPIController/index.js
@@ -26,7 +26,7 @@ async function getDogWalkInfoController(request, response) {
 
 async function addAdditionalRouteMarkersController(request, response) {
     const collection = await DbService.connectToDb();
-    await DbService.addAdditionalRouteMarkers(collection, request.params.id, request.params.markersArray);
+    await DbService.addAdditionalRouteMarkers(collection, request.body.id, request.body.markersArray);
 }
 
 module.exports.getAllStartMarkersController = getAllStartMarkersController;
