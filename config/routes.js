deaedfdd62cfb314891ee22d17cb3a2da7ad7fd9
jesshamby <jesshamby99@gmail.com>
2021-12-16 09:21:24 +0000

    Tidied up the routes

--- config/routes.js
+++ config/routes.js
@@ -1,20 +1,19 @@
-
 const MarkerAPIController = require("../src/Controllers/MarkerAPIController");
 const AddNewWalkController = require("../src/Controllers/AddNewWalkController");
-const ErrorController = require("../src/Controllers/ErrorController")
+const ErrorController = require("../src/Controllers/ErrorController");
 
 function routes(app){
     app.get('/markers', MarkerAPIController.getAllStartMarkersController);
     app.post('/markers', ErrorController);
-    app.put('/markers', ErrorController)
-    app.delete('/markers', ErrorController)
+    app.put('/markers', ErrorController);
+    app.delete('/markers', ErrorController);
+
+    app.get('/markers/:id', MarkerAPIController.getDogWalkInfoController);
 
     app.get('/walks', ErrorController);
     app.post('/walks', AddNewWalkController);
     app.put('/walks', ErrorController);
     app.delete('/walks', ErrorController);
-
-    app.get('/markers/:id', MarkerAPIController.getDogWalkInfoController);
 }
 
 module.exports = routes;
